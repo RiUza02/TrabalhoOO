87359d7d17edbc67c5f1f8805eece2809e7215d8
Rafaela-SCunha <104382934+Rafaela-SCunha@users.noreply.github.com>
2022-06-02 21:35:57 -0300

    Update trabalho.java

--- src/main/java/trabalho.java
+++ src/main/java/trabalho.java
@@ -3,15 +3,26 @@ public class trabalho {
 
     public static void main(String[] args) {
         Jogo jogo = new Jogo();
+        int comecar;
         int cont = 0;
-        
-        while(jogo.Encerrou(cont) != true){
-            jogo.imprime();
-            jogo.atirar();
-            cont++;
+        System.out.println("Bem Vindo ao Jogo Batalha Naval!!");
+        jogo.jogo();
+        System.out.println("Para começar o jogo digite 1");
+        Scanner teclado = new Scanner(System.in);
+        comecar = teclado.nextLine();
+        if(comecar == 1)
+        {
+            while ( jogo . Encerrou ( cont ) != true )
+            {
+                jogo . imprime ();
+                jogo . atirar ();
+                cont ++;
+                System.out.println("Você ainda tem" + jogo.getJogadas()"jogadas");
+            }
         }
-        
-        jogo.imprime();
-        System.out.println("FIM DE JOGO");
+        else
+        {
+            System.out.println("Comando inválido!!");
+        }
     }
 }
